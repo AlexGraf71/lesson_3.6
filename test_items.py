f3ed146c0c3271d031fc967ac288a82ff59794c0
AlexGraf <lexraf@list.ru>
2020-04-21 00:49:20 +0300

    Small fix

--- test_items.py
+++ test_items.py
@@ -1,17 +1,11 @@
 import time
-import math
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_availability_basket(browser):
     browser.get(link)
-    time.sleep(30)
+    time.sleep(10)
     button_text = 'Добавить в корзину'
     actual_button_text = browser.find_element_by_css_selector('.btn-add-to-basket').text
-    assert button_text == actual_button_text
+    assert button_text == actual_button_text, f"expected {button_text}, got {actual_button_text}"
